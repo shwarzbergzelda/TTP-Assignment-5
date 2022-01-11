c0cd099b3875741d635b4efb180a6b6f3af38691
zelda <zeldashw@gmail.com>
2022-01-11 03:15:08 -0500

    resolves #1: implemented logic for click-and-drag fill of grid cells. Styled page. Inserted comments to indicate which user feature is implemented in each code snippet/ function

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const rowItem = "<div class='grid-item-row'></div>";
 const colItem = "<div class='grid-item-col'></div>";
+const fillEmptyButton = document.getElementById("fillEmpty");
+const fillAllButton = document.getElementById("fillAll");
+const clearAllButton = document.getElementById("clearAll");
+const inputRows = document.getElementById("inputRows");
+const inputCols = document.getElementById("inputCols");
+const removeRows = document.getElementById("removeRows");
+const removeCols = document.getElementById("removeCols");
 
 const grid = document.getElementsByClassName("grid")[0];
 let size = "";
 let numOfRows = 0;
 let numOfColumns = 0;
 
+/* ADD ROW */
+inputRows.addEventListener("click", addRow);
 function addRow() {
     if (numOfColumns == 0) {
         numOfColumns = 1;
@@ -21,9 +30,11 @@ function addRow() {
         grid.innerHTML += colItem;
     }
     numOfRows++;
-    printResults();
 }
 
+
+/* ADD COLUMN */
+inputCols.addEventListener("click", addCol)
 function addCol() {
     size += " 50px";
     grid.style.gridTemplateColumns = size;
@@ -35,10 +46,13 @@ function addCol() {
         for (let i = 0; i < numOfRows; i++) {
             grid.innerHTML += colItem;
         }
-    } numOfColumns++;
-    printResults();
+    }
+    numOfColumns++;
 }
 
+
+/* REMOVE ROW */
+removeRows.addEventListener("click", removeRow);
 function removeRow() {
     let items = grid.children;
 
@@ -58,10 +72,11 @@ function removeRow() {
         numOfColumns = 0;
         resetGrid();
     }
-    console.log(items);
-    printResults();
 }
 
+
+/* REMOVE COLUMN */
+removeCols.addEventListener("click", removeCol);
 function removeCol() {
     let items = grid.children;
     const len = items.length;
@@ -85,44 +100,40 @@ function removeCol() {
         numOfRows = 0;
         resetGrid();
     }
-    console.log(items);
-    printResults();
 }
 
+
+/* RESET GRID TO CONTAIN NOTHING WHEN REMOVING LAST ROW/COLUMN */
 function resetGrid() {
     grid.innerHTML = "";
     size = "";
     grid.style.gridTemplateColumns = size;
 }
 
-function printResults() {
-    console.log("rows: " + numOfRows + "\ncols: " + numOfColumns);
-}
 
+/* RETRIEVES SELECTED COLOR FROM CLICKDOWN MENU */
 function getColor() {
     const color = document.getElementById("colors");
     return color.value;
 }
 
-grid.addEventListener("click", (e) => {
-    if ((e.target.getElementsByClassName("grid-item-col") || e.target.getElementsByClassName("grid-item-row")) && e.target !== grid) {
-        e.target.style.backgroundColor = getColor();
-    }
-});
 
-const fillEmptyButton = document.getElementById("fillEmpty");
-
+/* FILL ALL EMPTY/WHITE GRID CELLS */
 fillEmptyButton.addEventListener("click", () => {
     const gridItems = grid.children;
 
     for (let item of gridItems) {
-        if (window.getComputedStyle(item).getPropertyValue("background-color") == "rgb(255, 255, 255)") {
+        if (
+            window.getComputedStyle(item).getPropertyValue("background-color") ==
+            "rgb(255, 255, 255)"
+        ) {
             item.style.backgroundColor = getColor();
         }
     }
 });
 
-const fillAllButton = document.getElementById("fillAll");
+
+/* FILLS ALL GRID CELLS, REGARDLESS OF CURRENT FILL STATE/COLOR */
 fillAllButton.addEventListener("click", () => {
     const gridItems = grid.children;
 
@@ -131,7 +142,8 @@ fillAllButton.addEventListener("click", () => {
     }
 });
 
-const clearAllButton = document.getElementById("clearAll");
+
+/* RESET GRID CELLS' COLORS TO WHITE */
 clearAllButton.addEventListener("click", () => {
     const gridItems = grid.children;
 
@@ -139,3 +151,37 @@ clearAllButton.addEventListener("click", () => {
         item.style.backgroundColor = "white";
     }
 });
+
+
+/* RETURNS TRUE IF TARGET IS GRID CELL */
+function isGridItem(e) {
+    return (
+        (e.target.getElementsByClassName("grid-item-col") ||
+            e.target.getElementsByClassName("grid-item-row")) &&
+        e.target !== grid
+    );
+}
+
+
+/* CLICK ON SINGLE BUTTON TO CHANGE COLOR */ 
+grid.addEventListener("click", (e) => {
+    if (isGridItem(e)) {
+        e.target.style.backgroundColor = getColor();
+    }
+});
+
+/* CLICK AND DRAG TO CHANGE GRID CELLS' COLORS */
+grid.addEventListener("mousedown", mouseDownListener, true);
+grid.addEventListener("mouseup", mouseUpListener, true);
+
+function mouseMoveListener(e) {
+    isGridItem(e) && (e.target.style.backgroundColor = getColor());
+} // Fill grid cells that mouse is moving over
+
+function mouseDownListener(e) {
+    grid.addEventListener("mousemove", mouseMoveListener, true);
+} // While mouse is down, call function to fill grid cells
+
+function mouseUpListener(e) {
+    grid.removeEventListener("mousemove", mouseMoveListener, true);
+} // Stop event listener to prevent it from filling cells after ceased click/drag
