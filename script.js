e2f78995758b9dd731fa5efb170cbac4ddf9bfd7
YahiaE <yahiae2002@gmail.com>
2022-01-10 20:05:34 -0500

    add javascript for row insertion

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+const rowItem = "<div class='grid-item-row'></div>";
+const colItem = "<div class='grid-item-col'></div>";
+
+const grid = document.getElementsByClassName("grid")[0];
+let size = "";
+let numOfRows = 0;
+let numOfColumns = 0;
+
+function addRow(){
+    if(numOfColumns == 0){
+        numOfColumns = 1;
+        size = "";
+        for(let i = 0; i < numOfColumns; i++){
+            size+=" 50px";
+        }
+        grid.style.gridTemplateColumns = size;
+    }
+    grid.innerHTML+=rowItem;
+
+    for(let i = 0; i < numOfColumns-1; i++){
+        grid.innerHTML+=colItem;
+    }
+    numOfRows++;
+}
