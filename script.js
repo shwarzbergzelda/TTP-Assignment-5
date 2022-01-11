1a3b833e09344ccf05ecb0a1dfeb137617a3f4aa
zelda <zeldashw@gmail.com>
2022-01-10 23:10:00 -0500

    resolves #5: added logic to allow user to click individual cells to fill

--- script.js
+++ script.js
@@ -6,83 +6,112 @@ let size = "";
 let numOfRows = 0;
 let numOfColumns = 0;
 
-function addRow(){
-    if(numOfColumns == 0){
-        numOfColumns = 1;
-        size = "";
-        for(let i = 0; i < numOfColumns; i++){
-            size+=" 50px";
-        }
-        grid.style.gridTemplateColumns = size;
+function addRow() {
+  if (numOfColumns == 0) {
+    numOfColumns = 1;
+    size = "";
+    for (let i = 0; i < numOfColumns; i++) {
+      size += " 50px";
     }
-    grid.innerHTML+=rowItem;
-
-    for(let i = 0; i < numOfColumns-1; i++){
-        grid.innerHTML+=colItem;
-    }
-    numOfRows++;
-}
-
-function addCol(){
-    
-    size+=" 50px";
     grid.style.gridTemplateColumns = size;
+  }
+  grid.innerHTML += rowItem;
 
-    if(numOfRows == 0){
-        numOfRows = 1;
-        grid.innerHTML+=rowItem;
-    } else {
-        let total = document.getElementsByClassName("grid-item-row").length;
-        for(let i = 0; i < total; i++){
-            grid.innerHTML+=colItem;
-        }
-    }
-
-    numOfColumns++;
+  for (let i = 0; i < numOfColumns - 1; i++) {
+    grid.innerHTML += colItem;
+  }
+  numOfRows++;
+  printResults();
 }
 
-function removeRow(){
-    let items = grid.children;
-    
-    for(let i = 0; i < numOfColumns; i++){
-        grid.removeChild(items[0]);
-    }
+function addCol() {
+  size += " 50px";
+  grid.style.gridTemplateColumns = size;
 
-    numOfRows--;
-    
-    if(numOfRows == 0){
-        numOfColumns = 0;
-        resetGrid();
+  if (numOfRows == 0) {
+    numOfRows = 1;
+    grid.innerHTML += rowItem;
+  } else {
+    for (let i = 0; i < numOfRows; i++) {
+      grid.innerHTML += colItem;
     }
-    console.log(items);
-    printResults();
+  }
+
+  numOfColumns++;
+  printResults();
 }
 
-function removeCol(){
-    let items = grid.children;
-    const len = items.length;
-    
-    for(let i = len-1; i >= numOfColumns-1; i-=numOfColumns){
-        grid.removeChild(items[i]);
-    }
+function removeRow() {
+  let items = grid.children;
 
-    size = size.substring(0,size.lastIndexOf(" "));
+  for (let i = 0; i < numOfColumns; i++) {
+    grid.removeChild(items[0]);
+  }
+
+  numOfRows--;
+
+  if (numOfRows < 0) {
+    numOfRows = 0;
+    size = "";
     grid.style.gridTemplateColumns = size;
+  }
 
-    numOfColumns--;
-
-    if(numOfColumns == 0){
-        numOfRows = 0;
-        resetGrid();
-    }
-    console.log(items);
-    printResults();
+  if (numOfRows == 0) {
+    numOfColumns = 0;
+    resetGrid();
+  }
+  console.log(items);
+  printResults();
 }
 
-function resetGrid(){
-    grid.innerHTML="";
+function removeCol() {
+  let items = grid.children;
+  const len = items.length;
+
+  for (let i = len - 1; i >= numOfColumns - 1; i -= numOfColumns) {
+    grid.removeChild(items[i]);
+  }
+
+  size = size.substring(0, size.lastIndexOf(" "));
+  grid.style.gridTemplateColumns = size;
+
+  numOfColumns--;
+
+  if (numOfColumns < 0) {
+    numOfRows = 0;
+    size = "";
+    grid.style.gridTemplateColumns = size;
+  }
+
+  if (numOfColumns == 0) {
+    numOfRows = 0;
+    resetGrid();
+  }
+  console.log(items);
+  printResults();
 }
 
-function printResults(){
-    console.log("rows: " + numOfRows + "\ncols: " + numOfColumns);
+function resetGrid() {
+  grid.innerHTML = "";
+  size = "";
+  grid.style.gridTemplateColumns = size;
 }
+
+function printResults() {
+  console.log("rows: " + numOfRows + "\ncols: " + numOfColumns);
+}
+
+function getColor() {
+  const color = document.getElementById("colors");
+  return color.value;
+}
+
+grid.addEventListener("click", (e) => {
+  if (
+    (e.target.getElementsByClassName("grid-item-col") ||
+      e.target.getElementsByClassName("grid-item-row")) &&
+    e.target !== grid
+  ) {
+    e.target.style.backgroundColor = getColor();
+  }
+});
