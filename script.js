e58f12e458ad9bccc3a6d191cedc3664be1cf07b
YahiaE <yahiae2002@gmail.com>
2022-01-10 21:25:33 -0500

    add all addition and removal functions & html

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const rowItem = "<div class='grid-item-row'></div>";
 const colItem = "<div class='grid-item-col'></div>";
 
@@ -6,6 +5,23 @@ const grid = document.getElementsByClassName("grid")[0];
 let size = "";
 let numOfRows = 0;
 let numOfColumns = 0;
+
+function addRow(){
+    if(numOfColumns == 0){
+        numOfColumns = 1;
+        size = "";
+        for(let i = 0; i < numOfColumns; i++){
+            size+=" 50px";
+        }
+        grid.style.gridTemplateColumns = size;
+    }
+    grid.innerHTML+=rowItem;
+
+    for(let i = 0; i < numOfColumns-1; i++){
+        grid.innerHTML+=colItem;
+    }
+    numOfRows++;
+}
 
 function addCol(){
     
@@ -23,4 +39,50 @@ function addCol(){
     }
 
     numOfColumns++;
+}
+
+function removeRow(){
+    let items = grid.children;
+    
+    for(let i = 0; i < numOfColumns; i++){
+        grid.removeChild(items[0]);
+    }
+
+    numOfRows--;
+    
+    if(numOfRows == 0){
+        numOfColumns = 0;
+        resetGrid();
+    }
+    console.log(items);
+    printResults();
+}
+
+function removeCol(){
+    let items = grid.children;
+    const len = items.length;
+    
+    for(let i = len-1; i >= numOfColumns-1; i-=numOfColumns){
+        grid.removeChild(items[i]);
+    }
+
+    size = size.substring(0,size.lastIndexOf(" "));
+    grid.style.gridTemplateColumns = size;
+
+    numOfColumns--;
+
+    if(numOfColumns == 0){
+        numOfRows = 0;
+        resetGrid();
+    }
+    console.log(items);
+    printResults();
+}
+
+function resetGrid(){
+    grid.innerHTML="";
+}
+
+function printResults(){
+    console.log("rows: " + numOfRows + "\ncols: " + numOfColumns);
 }
